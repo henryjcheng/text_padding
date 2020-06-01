c28b795aabe2de2772061e3d143839c74cb31eb2
henry <henryjcheng@gmail.com>
2020-05-31 22:25:34 -0700

    initial commit

---
+++ train_cnn.py
@@ -0,0 +1,9 @@
+__author__ = "Henry Cheng"
+__email__ = "henryjcheng@gmail.com"
+__status__ = "dev"
+"""
+This module contains codes to train CNN multi-class classification model.
+
+We will start with simple 3 FC layers and focus on getting pipeline running before expanding 
+our architecture.
+"""
