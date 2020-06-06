bb3d5784691ba29d2af2a77dd96bbe1f07f7c294
henry <henryjcheng@gmail.com>
2020-06-06 10:35:22 -0700

    fix bug: zero_padding_random dimension mismatch

--- test_3fc.py
+++ test_3fc.py
@@ -9,7 +9,7 @@ Future dev:
     1. combine this into train_3fc
     .py
 """
-
+import random
 import numpy as np
 import pandas as pd
 from nltk.tokenize import word_tokenize
@@ -41,6 +41,40 @@ def zero_padding(list_to_pad, max_length, pad_dimension):
         list_to_pad = np.append(list_to_pad, vector_pad, axis=0)
         iteration += 1
     
+    return list_to_pad
+
+def zero_padding_random(list_to_pad, max_length, pad_dimension):
+    """
+    This function takes a list and add list of zeros until max_length is reached.
+    The number of zeroes in added list is determined by pad_dimension, which is the 
+    same as the dimension of the word2vec model.
+
+    Padding is done randomly, ie padding verctors are inserted into text randomly.
+    1. randomly generate a list of index for padding vectors
+    2. fill an empty numpy array with dimension = max_length
+
+    This function is intended to handle one list only so it can be passed 
+    into a dataframe as a lambda function.
+    """
+    # find number of padding vector needed
+    num_pad = max_length - len(list_to_pad)
+
+    # vector_pad = np.zeros(pad_dimension)
+    vector_pad = np.asarray([0] * pad_dimension, dtype=np.float32)
+    vector_pad = [vector_pad]    # convert to list of np.ndarray so we can append together 
+
+    random.seed(1)
+    position_random = random.sample(range(0, max_length-1), num_pad)
+    index_list_to_pad = 0
+    list_temp = np.empty((0, pad_dimension), dtype=np.float32)
+    for position in range(max_length):
+        if position in position_random:
+            vector_to_append = vector_pad
+        else:
+            vector_to_append = list_to_pad[index_list_to_pad]
+            index_list_to_pad += 1
+        list_temp = np.append(list_temp, vector_to_append, axis=0)
+
     return list_to_pad
 
 def zero_padding_bothside(list_to_pad, max_length, pad_dimension):
