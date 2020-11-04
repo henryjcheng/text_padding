a2aaaa442825c4ac8369cc79a3f9d7c484cc20ec
henry <henryjcheng@gmail.com>
2020-11-04 13:14:47 -0800

    add amazon_review_polarity

--- nets.py
+++ nets.py
@@ -27,6 +27,11 @@ class multilayer_perceptron(nn.Module):
             self.fc1_out = 120           # same as fc2_in
             self.fc2_out = 50            # same as fc3_in
             self.fc3_out = 14            # same as number of classes
+        elif dataset == 'amazon_review_polarity':
+            self.fc1_in = 657 * 50 * 1   
+            self.fc1_out = 120           # same as fc2_in
+            self.fc2_out = 50            # same as fc3_in
+            self.fc3_out = 2             # same as number of classes
         else:
             raise ValueError(f'Dataset: {dataset} not recognized.')
 
@@ -56,6 +61,9 @@ class CNN(nn.Module):
         elif dataset == 'dbpedia_ontology':
             self.fc1_in = 407 * 1
             self.n_class = 14
+        elif dataset == 'amazon_review_polarity':
+            self.fc1_in = 651 * 1
+            self.n_class = 2
         else:
             raise ValueError(f'Dataset: {dataset} not recognized.')
 
@@ -88,6 +96,9 @@ class CNN_kim(nn.Module):
         elif dataset == 'dbpedia_ontology':
             self.fc1_in = 408
             self.n_class = 14
+        elif dataset == 'amazon_review_polarity':
+            self.fc1_in = 652
+            self.n_class = 2
         else:
             raise ValueError(f'Dataset: {dataset} not recognized.')
 
@@ -142,6 +153,12 @@ class CNN_deep(nn.Module):
             self.kernel4 = 49
             self.fc1_in = 47
             self.n_class = 14
+        elif dataset == 'yelp_review_polarity':
+            self.kernel2 = 327
+            self.kernel3 = 163
+            self.kernel4 = 81
+            self.fc1_in = 79
+            self.n_class = 2
         else:
             raise ValueError(f'Dataset: {dataset} not recognized.')
 
--- w2v/train_w2v.py
+++ w2v/train_w2v.py
@@ -88,6 +88,9 @@ if __name__ == "__main__":
     elif dataset == 'dbpedia_ontology':
         df = pd.read_csv(data_path, header=0, names=['label', 'title', 'text'])
         df_text = df[['text']].reset_index(drop=True)
+    elif dataset == 'amazon_review_polarity':
+        df = pd.read_csv(data_path, header=0, names=['label', 'title', 'text'])
+        df_text = df[['text']].reset_index(drop=True)
     else:
         print(f'Dataset: {dataset} not recognized.')
     
